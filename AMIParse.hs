bdeb06fd3556a342cc335f7adc9af7ae0317b74c
capn-freako <capn.freako@gmail.com>
2011-08-31 01:04:35 +0000

    Revert to monadic parsing code, as applicative code is not working correctly. (It's omitting last block.)
    git-svn-id: file:///home/dbanas/.svnrepos/prj/haskell/AMIParse/trunk@67 a3060f21-5d2f-4fa9-b7e5-c9f0903dceb1

--- AMIParse.hs
+++ AMIParse.hs
@@ -1,6 +1,6 @@
 module AMIParse where
 
-import ApplicativeParsec
+import Text.ParserCombinators.Parsec
 import Data.List
 
 type AmiToken = (String, AmiExp)
@@ -11,24 +11,32 @@ instance Show AmiExp where
     show = showTree ""
 
 showTree :: String -> AmiExp -> String
-showTree _ (Vals strs)        = " " ++ intercalate " " strs ++ "\n"
-showTree indent (Tokens toks) = '\n' : concatMap (showToken ('\t' : indent)) toks
+showTree _ (Vals strs)        = " " ++ (intercalate " " strs) ++ "\n"
+showTree indent (Tokens toks) = "\n" ++ (concat $ map (showToken ('\t' : indent)) toks)
 
 showToken :: String -> AmiToken -> String
-showToken indent tok = indent ++ "(" ++ fst tok ++ showTree indent (snd tok) ++ indent ++ ")\n"
+showToken indent tok = indent ++ "(" ++ (fst tok) ++ (showTree indent (snd tok)) ++ indent ++ ")\n"
 
 -- This is the AMI specific parser.
 amiToken :: Parser AmiToken
-amiToken = liftA2 (,) (skipJunk *> symbol (char '(') *> symbol (identifier <?> "label"))
-                      (try (Tokens <$> symbol (many1 amiToken))
-                       <|> Vals <$> (quotedVal <|> many (noneOf " )")) `sepBy` char ' ' <* symbol (char ')'))
+amiToken = do skipJunk
+              symbol (char '(')
+              lbl <- symbol (identifier <?> "label")
+              do tokens <- try (symbol (many1 amiToken))
+                 symbol (char ')')
+                 return (lbl, Tokens tokens)
+               <|> do vals <- sepBy (quotedVal <|> many (noneOf " )")) (char ' ')
+                      symbol (char ')')
+                      return (lbl, Vals vals)
 
 -- These are helper functions.
 identifier :: Parser String
 identifier = many1 (alphaNum <|> char '_')
 
 real :: Parser Double
-real = read <$> many1 (digit <|> char '+' <|> char '-' <|> char '.')
+real = do
+    n <- many1 (digit <|> char '+' <|> char '-' <|> char '.')
+    return (read n)
 
 eol :: Parser Char
 eol = char '\n'
@@ -37,15 +45,24 @@ whitespace :: Parser String
 whitespace = many1 space
 
 skipJunk :: Parser ()
-skipJunk = skipMany space *> skipMany (char '|' *> anyChar `manyTill` eol *> skipMany space)
+skipJunk = do
+    skipMany space
+    skipMany (do char '|'
+                 manyTill anyChar eol
+                 skipMany space)
+    return ()
 
 symbol :: Parser a -> Parser a
-symbol p = p <* skipJunk
+symbol p = do res <- p
+              skipJunk
+              return res
 
 quotedVal :: Parser String
-quotedVal = char '"' *> liftA quoteTok (many (satisfy (/= '"'))) <* char '"'
-quoteTok :: String -> String
-quoteTok s = '"' : s ++ "\""
+quotedVal = do
+    char '"'
+    res <- many (satisfy (/= '"'))
+    char '"'
+    return $ '"' : res ++ "\""
 
 getAmiExp :: AmiToken -> [String] -> Maybe AmiExp
 getAmiExp _ [] = Nothing
